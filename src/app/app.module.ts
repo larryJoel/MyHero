ca49c9451a9364f2e0e30c303f93136049e5767a
Larry Gonzalez <larry.g.m1024@gmail.com>
2019-12-29 07:32:38 -0300

    iniciando el respaldo git

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+//servicives
+
+//routing
+import { app_routing } from "./app.routes";
+
+//components
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    app_routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
